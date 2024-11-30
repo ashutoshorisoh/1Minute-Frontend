59b296df753a48dc76baaeed7905206938d4e456
Ashutosh <ashutoshorisoh@gmail.com>
2024-11-30 15:39:51 +0530

    Remove all files

--- src/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-// ProtectedRoute.js
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ isAuthenticated, children }) => {
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
